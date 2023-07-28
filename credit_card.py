1ea062baa95acf01afaa3dc560715002c3776d91
Louise E. Sinks <lsinks@users.noreply.github.com>
2023-07-28 14:50:55 -0400

    redoing credit card fraud in python

---
+++ credit_card.py
@@ -0,0 +1,4 @@
+import pandas as pd
+credit_card = pd.read_csv("credit_card_fraud.csv")
+credit_card.describe()
+credit_card.info()
---
+++ penguins.py
@@ -0,0 +1,3 @@
+from r-palmerpenguins import load_penguins
+penguins = load_penguins()
+penguins.describe()
